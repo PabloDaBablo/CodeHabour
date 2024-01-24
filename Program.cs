2c3b4841d5d7c3183ef9c33cba9acaf806a54fc6
Michael <mdionne1@ncstudents.niagaracollege.ca>
2024-01-23 19:55:56 -0500

    setup project added sqllite and other nuget packages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI;
+using WMBA_7_2_.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.
+    GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection string 'Default Connection' was not found");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddDbContext<WMBAContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
