841e2299bd6584781d932d6491a2021291e7a08b
lWillis19 <leo.williston19@gmail.com>
2024-01-27 18:18:40 -0500

    made a new file in the controllers class

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WMBA_7_2_.Models;
+
+namespace WMBA_7_2_.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly ILogger<TeamController> _logger;
+
+        public TeamController(ILogger<TeamController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
