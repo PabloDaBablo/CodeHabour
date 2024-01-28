440eece820cbc8c71097582d732afa8ee52f8a5b
donaven <jcarmichael3@ncstudents.niagaracollege.ca>
2024-01-27 22:48:52 -0500

    Added Player Controller

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WMBA_7_2_.Data;
+using WMBA_7_2_.Models;
+
+namespace WMBA_7_2_.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly WMBAContext _context;
+
+        public PlayerController(WMBAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Player
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Players.ToListAsync());
+        }
+
+        // GET: Player/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Players == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // GET: Player/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Player/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,PlayerMemberID,PlayerFirstName,PlayerLastName,PlayerNumber,MemberID")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(player);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(player);
+        }
+
+        // GET: Player/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Players == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Player/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,PlayerMemberID,PlayerFirstName,PlayerLastName,PlayerNumber,MemberID")] Player player)
+        {
+            if (id != player.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(player);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlayerExists(player.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(player);
+        }
+
+        // GET: Player/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Players == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // POST: Player/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Players == null)
+            {
+                return Problem("Entity set 'WMBAContext.Players'  is null.");
+            }
+            var player = await _context.Players.FindAsync(id);
+            if (player != null)
+            {
+                _context.Players.Remove(player);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlayerExists(int id)
+        {
+          return _context.Players.Any(e => e.ID == id);
+        }
+    }
+}
