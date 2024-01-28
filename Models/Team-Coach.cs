707108e0adffa22420703e1a49d3dc4621a2674d
GTanWan <142838774+GTanWan@users.noreply.github.com>
2024-01-27 19:17:54 -0500

    Team-Coach.cs

--- Models/Team-Coach.cs
+++ Models/Team-Coach.cs
@@ -1,7 +1,11 @@
-﻿namespace WMBA_7_2_.Models
+﻿using System.Diagnostics.Contracts;
+
+namespace WMBA_7_2_.Models
 {
     public class Team_Coach
     {
-
+        public int Id { get; set; }
+        public int TeamId { get; set; }
+        public int CoachId { get; set; }
     }
 }
