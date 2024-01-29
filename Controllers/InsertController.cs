9d3ee374967456570d4c32cfb5227cbf529c9884
lWillis19 <leo.williston19@gmail.com>
2024-01-29 10:07:24 -0500

    New controller for installing outside items

---
+++ Controllers/InsertController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WMBA_7_2_.Models;
+
+namespace WMBA_7_2_.Controllers
+{
+    public class InsertController : Controller
+    {
+        private readonly ILogger<InsertController> Logger;
+
+        public InsertController(ILogger<InsertController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
+
+	
