0bc55b1a51ce13bf32f92557d730628db1bf306f
donaven <jcarmichael3@ncstudents.niagaracollege.ca>
2024-01-29 20:48:34 -0500

    About page created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace WMBA_7_2_.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
