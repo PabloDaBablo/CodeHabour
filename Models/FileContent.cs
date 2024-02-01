378012929c4e508206fdfbf3ec51f5f11eeed180
donaven <jcarmichael3@ncstudents.niagaracollege.ca>
2024-01-31 23:58:53 -0500

    Added FileContent.cs and UploadedFiles.cs

---
+++ Models/FileContent.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WMBA_7_2_.Models
+{
+    public class FileContent
+    {
+        [Key, ForeignKey("UploadedFile")]
+        public int FileContentID { get; set; }
+
+        [ScaffoldColumn(false)]
+        public byte[] Content { get; set; }
+
+        public UploadedFile UploadedFile { get; set; }
+    }
+}
+
