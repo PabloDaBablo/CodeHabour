8c146b0198c0f15c989385fa4201a38b55141ad9
donaven <jcarmichael3@ncstudents.niagaracollege.ca>
2024-03-06 23:57:04 -0500

    Fixed Game Time validation and renamed/moved Documents

--- Models/Game.cs
+++ Models/Game.cs
@@ -8,6 +8,16 @@ namespace WMBA_7_2_.Models
         Home,
         Away
     }
+    public enum Ballparks
+    {
+        ChippawaParkDiamond,
+        MapleParkDiamond1,
+        MapleParkDiamond2,
+        MemorialParkDiamond2,
+        BurgerParkDiamond,
+        MemorialParkDiamond3,
+        WellandJackfishStadium
+    }
 
 
     public class Game : IValidatableObject
@@ -24,12 +34,6 @@ namespace WMBA_7_2_.Models
         [DisplayFormat(ApplyFormatInEditMode = true , DataFormatString = "{0:HH:mm}")]
         [Required(ErrorMessage = "Time cannot be empty!")]
         public DateTime GameTime { get; set; }
-
-        //I dont know if we need this or not. ~donaven
-        //[Display(Name = "Season")]
-        //[Required(ErrorMessage = "Season cannot be empty!")]
-        //[StringLength (50, ErrorMessage = "Season cannot be longer than 50 characters")]
-        //public string GameSeason { get; set; }
 
         [Display(Name = "Location")]
         [Required(ErrorMessage = "Location cannot be empty!")]
@@ -66,22 +70,17 @@ namespace WMBA_7_2_.Models
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             //Games can't be played in the past
-            if (GameDate < DateTime.Now)
+            DateTime checkDateTime = GameDate.Date.Add(GameTime.TimeOfDay);
+            if (checkDateTime <= DateTime.Now.AddSeconds(1))
             {
-                yield return new ValidationResult("Games must occur in the future", new[] { "GameDate" });
+                yield return new ValidationResult("Game must occur in the future.", new[] { "GameDate", "GameTime" });
             }
-            //Games are played during the day???
-            if(GameTime.TimeOfDay < TimeSpan.FromHours(9) || GameTime.TimeOfDay >= TimeSpan.FromHours(20))
+            //Games are played during the day
+            if (GameTime.TimeOfDay < TimeSpan.FromHours(9) || GameTime.TimeOfDay >= TimeSpan.FromHours(20))
             {
                 yield return new ValidationResult("Games must be scheduled after 9 am and before 8 pm.", new[] { "GameTime" });
             }
-            //Games are played at an approved field
-            //if (GameLocation != "Chippawa Park Diamond" || GameLocation != "Maple Park Diamond #1" || GameLocation != "Maple Park Diamond #2"
-            //    || GameLocation != "Memorial Park Diamond #2" || GameLocation != "Burger Park Diamond" || GameLocation != "Memorial Park Diamond #3"
-            //    || GameLocation != "Welland Jackfish Stadium")
-            //{
-            //    yield return new ValidationResult("Games must be scheduled at an appropriate field.", new[] { "GameLocation" });
-            //}
+       
             //Home team and Visiting team can't be the same team
             if (HomeTeamID == AwayTeamID)
             {
