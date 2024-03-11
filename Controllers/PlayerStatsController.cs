3f70401ab507e283146dab9c5830c050eca4e166
donaven <jcarmichael3@ncstudents.niagaracollege.ca>
2024-03-11 15:22:39 -0400

    added sorting to PlayerStats page

--- Controllers/PlayerStatsController.cs
+++ Controllers/PlayerStatsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WMBA_7_2_.Data;
 using WMBA_7_2_.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WMBA_7_2_.Controllers
 {
@@ -20,9 +21,84 @@ namespace WMBA_7_2_.Controllers
         }
 
         // GET: PlayerStats
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string actionButton, string sortDirection = "asc", string sortField = "Runs")
         {
-            var wMBAContext = _context.PlayerStats.Include(p => p.Player);
+            var wMBAContext = _context.PlayerStats.Include(p => p.Player).AsNoTracking();
+
+            //List of sort options.
+            string[] sortOptions = new[] { "Runs", "Hits", "Home Runs", "RBI" };
+
+            //if (!String.IsNullOrEmpty(sortDirection)) 
+            //{
+            //the String has an ambiguity error which I cant fix,
+            //but the sort works, so I will deal with it with Stovell later. 
+            //~donaven
+            if (sortOptions.Contains(actionButton))
+                {
+                    if (actionButton == sortField)
+                    {
+                        sortDirection = sortDirection == "asc" ? "desc" : "asc";
+                    }
+                    sortField = actionButton;
+                }
+            //}
+
+            if (sortField == "Runs")
+            {
+                if (sortDirection == "asc")
+                {
+                    wMBAContext = wMBAContext
+                        .OrderBy(p => p.Runs);
+                }
+                else
+                {
+                    wMBAContext = wMBAContext
+                        .OrderByDescending(p => p.Runs);
+                }
+            }
+            else if (sortField == "Hits")
+            {
+                if (sortDirection == "asc")
+                {
+                    wMBAContext = wMBAContext
+                        .OrderByDescending(p => p.Hits);
+                }
+                else
+                {
+                    wMBAContext = wMBAContext
+                        .OrderBy(p => p.Hits);
+                }
+            }
+            else if (sortField == "Home Runs")
+            {
+                if (sortDirection == "asc")
+                {
+                    wMBAContext = wMBAContext
+                        .OrderBy(p => p.HR);
+                }
+                else
+                {
+                    wMBAContext = wMBAContext
+                        .OrderByDescending(p => p.HR);
+                }
+            }
+            else
+            {
+                if (sortDirection == "asc")
+                {
+                    wMBAContext = wMBAContext
+                        .OrderBy(p => p.RBI);
+                }
+                else
+                {
+                    wMBAContext = wMBAContext
+                        .OrderByDescending(p => p.RBI);
+                }
+            }
+            //Set sort for next time
+            ViewData["sortField"] = sortField;
+            ViewData["sortDirection"] = sortDirection;
+
             return View(await wMBAContext.ToListAsync());
         }
 
